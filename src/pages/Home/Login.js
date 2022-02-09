154c5b7b341ebac370200d95a5aefba73f8415b3
Weiting <boterasuo@gmail.com>
2022-02-09 22:44:34 +0800

    2/9 first commit of login pages

---
+++ src/pages/Home/Login.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import Logo from "../../img/LOGO_no_word.svg";
+import LogoWord from "../../img/SignUp/LOGO_word.svg";
+import LoginWord from "../../img/login_word.svg";
+import { ReactComponent as PawBtn } from "../../img/paw_btn.svg";
+import "./Login.scss";
+import { BsGoogle, BsFacebook } from "react-icons/bs";
+
+function Login(props) {
+  return (
+    <div className="main-content ">
+    <div className="login-space-section">
+      <div className="container">
+          <div className="row no-gutters">
+              <div className="col-lg-6 d-none d-lg-block"></div>
+              <div className="col-lg-4">
+                <div className="login-logo-word">
+                  <img alt="" className="img-fluid" src={LogoWord}/>
+                </div>
+                <h4 className="text-secondary text-center py-3 m-0">It's Mao Life!</h4>
+                <form className="login-form">
+                  <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="帳號" />
+                  </div>
+                  <div className="input-group mb-3">
+                      <input type="password" className="form-control" placeholder="密碼" />
+                  </div>
+                </form>
+              </div>
+              <div className="col-lg-2 align-self-end">
+                <button className="btn login-btn">
+                  <PawBtn className="paw-btn" />
+                </button>
+              </div>
+          </div>
+      </div>
+        
+    </div>
+    <div className='bg-primary position-relative login-section'>       
+        <div className="container"> 
+            {/* logo 大圖  */}
+            <div className="login-logo-noword position-absolute">
+                <img alt="" className="img-fluid" src={Logo}/>
+            </div>
+            {/* 會員登入文字圖 */}
+            <div className="position-absolute d-none d-lg-block img-login-word">
+                <img alt="" className="img-fluid" src={LoginWord}/>
+            </div>
+            <div className="row">
+                {/* 排版用區塊 */}
+                <div className="col-lg-6"></div>
+                {/* 右方第三方登入區塊 */}
+                <div className="col-lg-4">
+                    <hr className="d-lg-none divider"/>
+                    <div className='d-flex w-100 thirdParty-login'>
+                        <div className="thirdParty-icon-login">
+                            <BsGoogle color="white" fontSize="2.5rem"/>
+                        </div>
+                        <div className="thirdParty-icon-login">
+                            <BsFacebook color="white" fontSize="2.5rem"/>
+                        </div>
+                    </div>                    
+                </div>
+                <div className="col-lg-2"></div>
+            </div>
+        </div>
+    </div>
+  </div>
+  );
+}
+
+
+export default Login;
