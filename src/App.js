8b50291e3e2996d26058c2365a982ba14725e685
syuan <66296723+dummiss@users.noreply.github.com>
2022-02-11 14:58:48 +0800

    更新App路由
    請自行加上各自的路由名稱
    container部分請自行在自己的入口包

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from 'react'
+import MyNav from "./component/UI/MyNav";
+import ScrollToTop from "./component/UI/ScrollToTop";
+import Store from "./pages/Store.js";
+
+
 function App() {
   return (
-    <div>
-      intial project 
-    </div>
-  );
+    <Router>
+      <>
+        <MyNav />
+          <ScrollToTop>
+            <Switch>
+              <Route path="/store">
+                <Store />
+              </Route>
+                <Home />
+            </Switch>
+          </ScrollToTop>
+      </>
+    </Router>
+  )
 }
 
 export default App;
