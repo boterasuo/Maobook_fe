f828bdd37df06b36983b601e16a1d33433cff944
Weiting <boterasuo@gmail.com>
2022-02-13 19:12:30 +0800

    update App.js: 新增 Home、Login、NotFound 頁面

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import React from 'react'
 // 引入頁面
 import Home from "./pages/Home";
+import Member from "./pages/Member";
+import Login from "./pages/Home/Login";
+import Store from "./pages/Store.js";
+import NotFound404 from "./pages/Home/NotFound404";
+
+// 引入元件
 import MyNav from "./component/UI/MyNav";
 import ScrollToTop from "./component/UI/ScrollToTop";
-import Store from "./pages/Store.js";
+import Footer from "./component/UI/Footer";
+
 
 
 function App() {
@@ -15,12 +22,24 @@ function App() {
         <MyNav />
           <ScrollToTop>
             <Switch>
+              <Route path="/member">
+                <Member />
+              </Route>
               <Route path="/store">
                 <Store />
               </Route>
+              <Route path="/login">
+                <Login/>
+              </Route>
+              <Route path="/" exact>
                 <Home />
+              </Route>
+              <Route path="*">
+                <NotFound404/>
+              </Route>
             </Switch>
           </ScrollToTop>
+          <Footer/>
       </>
     </Router>
   )
