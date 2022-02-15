bc0646afb14385fc44596f0ed86e68c33d88783e
Chris <chris@HT.local>
2022-02-15 16:10:35 +0800

    [0215] first commit : main structure
    尚未切版

---
+++ src/pages/Assistance/DayHelps.js
@@ -0,0 +1,63 @@
+
+import React from 'react';
+
+function DayHelps(props) {
+  return (
+    <>
+
+      <div className="container">
+      
+        <div className="">2022/03/11</div>
+
+      </div>
+
+        <div className="container" style={ { border: "2px orange solid"} }> 
+
+        <div className="first" style={ { display:"flex"} }>
+
+        <div className="">3/11</div>
+
+        <div className="main" style={ { border: "1px orange solid", display:"flex"}}>
+
+        <div style={ { border: "1px solid"}}>台北市</div>
+        <div style={ { border: "1px solid"}}>狗狗</div>
+        <div style={ { border: "1px solid"}}>代遛</div>
+        <div style={ { border: "1px solid"}}>NT$1000</div>
+        <div>標題標題標題標題標題標題</div>
+        <div>（箭頭按鈕）</div>
+
+        </div>
+        
+        <div className="">(爪)＿人應徵</div>
+
+        </div>
+
+
+        <br/>
+
+        <div className="second" style={ { display:"flex"} }>
+
+        <div className="">3/12</div>
+        
+        <div className="main" style={ { border: "1px orange solid", display:"flex"}}>
+
+        <div style={ { border: "1px solid"}}>桃園市</div>
+        <div style={ { border: "1px solid"}}>貓貓</div>
+        <div style={ { border: "1px solid"}}>代遛</div>
+        <div style={ { border: "1px solid"}}>NT$2000</div>
+        <div>標題標題標題標題標題標題</div>
+        <div>（箭頭按鈕）</div>
+
+        </div>
+        
+        <div className="">(爪)＿人應徵</div>
+
+        </div>
+        </div>
+
+  </>
+  )
+}
+
+
+export default DayHelps
---
+++ src/pages/Assistance/HelpDetail.js
@@ -0,0 +1,48 @@
+
+import React from 'react';
+
+function HelpDetail(props) {
+  return (
+    <>
+
+      <div className="container">
+      
+        <div className="lefeside">左img</div><br/>
+
+        <div className="rightside">
+
+        <div className="righthead">
+        <div>使用者</div><hr/>
+        </div>
+
+        <div className="rightbody1">
+        <div>地區 標籤</div><br/>
+        <div>標題</div><br/>
+        <div>時間</div><br/>
+        <div>金額</div><br/>
+        <div>內文</div><hr/>
+        </div>
+
+        <div className="rightfoot" style={ { display:"flex"} }>
+        
+        <div>爪圖案</div>
+        <div>
+        <div>聯絡方式</div><br/>
+        <div>我要留言</div><br/>
+        <div>送出按鈕</div>
+        </div>
+
+        </div>
+        
+        <hr/>
+        <div className="">分享按鈕</div>
+
+        </div>
+        </div>
+   
+  </>
+  )
+}
+
+
+export default HelpDetail
