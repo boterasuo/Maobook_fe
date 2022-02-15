e680b216ad150242c293f2a81f31526ebd519ef9
Anun <84383897+Ben-Buli@users.noreply.github.com>
2022-02-15 16:09:34 +0800

    build default content

---
+++ src/pages/Store.js
@@ -0,0 +1,13 @@
+// 預設格式：請自改為自己的內容
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Store(props) {
+  return (
+    <div>Store</div>
+  )
+}
+
+Store.propTypes = {}
+
+export default Store
