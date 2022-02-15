397b4f06546f27337e46ac145306916f9591a314
Anun <84383897+Ben-Buli@users.noreply.github.com>
2022-02-15 16:10:30 +0800

    Update Store.js

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,13 +1,1 @@
-// 預設格式：請自改為自己的內容
-import React from 'react'
-import PropTypes from 'prop-types'
 
-function Store(props) {
-  return (
-    <div>Store</div>
-  )
-}
-
-Store.propTypes = {}
-
-export default Store
