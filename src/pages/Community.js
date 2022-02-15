eae8c7d87f95fa394152e1f19366dddf4078555a
Eutene Lin <eutenelin@gmail.com>
2022-02-16 03:29:39 +0800

    schedule update

---
+++ src/pages/Community.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Community(props) {
+  return (
+    <div>Community</div>
+  )
+}
+
+Community.propTypes = {}
+
+export default Community
---
+++ src/pages/Schedule/Intro.js
@@ -0,0 +1,71 @@
+// 引入 React 功能
+import React from 'react';
+// import {useState} from "react";
+// import axios from 'axios';
+// import { useHistory } from "react-router-dom";
+
+// 引入 utils
+// import {API_URL} from "../../utils/config";
+import 'react-bootstrap'
+import '../Schedule/Intro.scss'
+
+// 引入圖片們
+import cheduleInfochat2 from '../Schedule/img/cheduleInfochat2.svg'
+import schedule2 from '../Schedule/img/schedule2.svg'
+
+function intro() {
+  
+  return (
+
+    <>
+
+      <div className="introdiv">
+        <div className="homePage"></div>
+
+        <div className="scheduleIntro">
+
+            <div className="IntroTop">
+              <div className="scheduleInfoTxt">
+                <h2>提醒您</h2>
+                <h2>毛孩生活的大小事</h2>
+                <h5></h5>
+                <h5>打疫苗、散散步</h5>
+                <h5>細心記錄下毛孩每一個生長狀況</h5>
+                <h5>讓毛孩健康長大</h5>
+              </div>
+
+              <div className="cheduleInfochat">
+                <img src={cheduleInfochat2} alt="" />
+              </div>
+
+            </div>
+
+            <div className="Introdowm">
+              <div className="chedulePhoto">
+                <img src={schedule2} alt="" />
+              </div>
+              <div className="arrowtext">
+
+              </div>
+
+            </div>
+
+          {/* <div className="cTxt">
+            <img src="./img/scheduleText1.svg" alt="" />
+          </div>  */}
+
+        </div>
+      </div>
+
+    </>
+    
+  )
+
+}
+
+
+
+
+
+
+export default intro;
