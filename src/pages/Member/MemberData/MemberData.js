b6e172a0e111c181b522954f59c0adc19b340cea
Weiting <boterasuo@gmail.com>
2022-02-17 23:55:34 +0800

    update member related pages

--- src/pages/Member/MemberData/MemberData.js
+++ src/pages/Member/MemberData/MemberData.js
@@ -1,35 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Table } from "react-bootstrap";
+import axios from 'axios';
 // 引入 user context
 import { useAuth } from '../../../context/auth';
 // 引入 utils
 import { API_URL } from '../../../utils/config';
 // 引入 icon
 import { BsPencilSquare } from "react-icons/bs";
-// 引入預設大頭照
+// 引入圖片
 import defaultAvatar from "../../../img/avatar_user.png";
-import axios from 'axios';
+import loading from "../../../img/loading_paw.svg";
 
 function MemberData(props) {
     const [avatar, setAvatar] = useState(defaultAvatar);
   // 來自 context 的 user 狀態
   const {user, setUser} = useAuth();
+  const [userInfo, setUserInfo] = useState();
 
   // 取得使用者詳細資料
-  let getUserInfo = async () => {
-    try {
-      let result = await axios.get(`${API_URL}/member/info`, {withCredentials: true,});
-      console.log(result.data);
-    } catch(e) {
-      console.error("user info 錯誤", e.response.data);
-    }
-  }
-  getUserInfo();
-  console.log(user);
+  useEffect(() => {
+    let getUserInfo = async () => {
+      try {
+        let result = await axios.get(`${API_URL}/member/info`, {withCredentials: true,});
+        console.log(result.data.data);
+        setUserInfo(result.data.data);
+      } catch(e) {
+        console.error("user info 錯誤", e.response.data);
+      }
+    };
+    getUserInfo();
+  }, []);
+
+  const loadingPaw = (
+    <div className="text-center">
+        <div className="spinner-grow text-primary" role="status">
+          <img alt="" className="sr-only" src={loading} />
+        </div>
+    </div>    
+  );
 
   return (
     <>
-        <div className="row position-relative">
+      { userInfo ? (
+        <div className="row position-relative info-card">
                 {/* 大頭照區域 */}
                 <div className="col-lg-5 w-100">
                   <div className="embed-responsive embed-responsive-1by1 avatar">
@@ -37,7 +50,7 @@ function MemberData(props) {
                   </div>
                   {/* 使用者姓名變數 */}
                   <div className="text-center pt-3 h4 text-secondary font-weight-bold">
-                    {user ? user.name : "處理中"}
+                    {userInfo.name ? userInfo.name : "未有資料"}
                   </div>
                 </div>
                 {/* 個人資料表格 */}
@@ -46,7 +59,9 @@ function MemberData(props) {
                     <tbody>
                       <tr>
                         <td>帳號</td>
-                        <td className="text-grey">{user ? user.email : "處理中"}</td>
+                        <td className="text-grey">
+                          {userInfo.email ? userInfo.email : "未有資料"}
+                        </td>
                       </tr>
                       <tr>
                         <td>密碼</td>
@@ -58,23 +73,25 @@ function MemberData(props) {
                       </tr>
                       <tr>
                         <td>性別</td>
-                        <td>男</td>
+                        <td>{userInfo.gender ? userInfo.gender : "尚未提供"}</td>
                       </tr>
                       <tr>
                         <td>手機</td>
-                        <td>0912-345-678</td>
+                        <td>{userInfo.mobile ? userInfo.mobile : "尚未提供"}</td>
                       </tr>
                       <tr>
                         <td>生日</td>
-                        <td>1999 / 12 / 25</td>
+                        <td>
+                          {userInfo.birthday ? userInfo.birthday : "尚未提供"}
+                        </td>
                       </tr>
                       <tr>
                         <td>信箱</td>
-                        <td>{user ? user.email : "處理中"}</td>
+                        <td>{userInfo.email ? userInfo.email : "尚未提供"}</td>
                       </tr>
                       <tr>
                         <td>地址</td>
-                        <td>桃園市中壢區聖德基督學院</td>
+                        <td>{userInfo.address ? userInfo.address : "尚未提供"}</td>
                       </tr>
                     </tbody>
                   </Table>
@@ -82,9 +99,9 @@ function MemberData(props) {
                 <button className="edit-icon" title="編輯會員資料">
                   <BsPencilSquare color="white" fontSize="1.3rem" />
                 </button>
-
-
             </div>
+      ) : loadingPaw }
+        
     </>
   )
 }
--- src/pages/Member/PetList/PetList.js
+++ src/pages/Member/PetList/PetList.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import loading from "../../../img/loading_paw.svg";
 
 function PetList(props) {
   return (
-    <div>pet list</div>
+      <div className="info-card">
+        <h2>
+          pet list
+        </h2>
+        {/* loading 動圖 (記得修改圖片路徑) */}
+        <div className="text-center">
+          <div className="spinner-grow text-primary" role="status">
+            <img alt="" className="sr-only" src={loading} />
+          </div>
+        </div>
+        
+      </div>
+
   )
 }
 
