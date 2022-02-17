fd52c5c3f56b6870b9f24449fcafaa7c37c67ad3
Sinmei <93106691+Jonawonabanana@users.noreply.github.com>
2022-02-17 16:31:20 +0800

    update MyNav.js and global.scss

--- src/component/UI/MyNav.js
+++ src/component/UI/MyNav.js
@@ -8,44 +8,103 @@ import {
   Container,
 } from "react-bootstrap";
 
-// scss
 import "../../style/UI/global.scss";
+import Logo from "../../img/Logo_nav.svg";
 
 // 要使用能有active css效果的NavLink元件
 import { NavLink } from "react-router-dom";
-
 
 function MyNav(props) {
   const { auth } = props;
 
   return (
     <>
-      <Navbar expand="lg" fixed="top" className="navbar text-center">
+      <Navbar
+        className="navbar text-center"
+        collapseOnSelect
+        expand="lg px-3"
+        fixed="top"
+      >
+      {/* Nav左側Logo */}
         <Navbar.Brand href="/">
-         MaoBook
+          <img className="navbar-logo mr-2" src={Logo} width="200" alt="Logo" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
+      {/* Nav中間 連結各平台頁面 */}
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Container>
             <Nav className="me-auto px-4">
-              {/* 連結各平台頁面 */}
-              <Nav.Link href="/home">Home</Nav.Link>
-              <Nav.Link href="/schedule">Schedule</Nav.Link>
-              <Nav.Link href="/store">Store</Nav.Link>
-              <Nav.Link href="/community">Community</Nav.Link>
-              <Nav.Link href="/assistance">Assistance</Nav.Link>
+              <Nav.Link as={NavLink} to="/">
+                HOME
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/schedule">
+                SCHEDULE
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/store">
+                STORE
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/community">
+                COMMUNITY
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/assistance">
+                ASSISTANCE
+              </Nav.Link>
             </Nav>
           </Container>
+
+        {/* Nav右側*/}
+          {/* 登入前 */}
+          {/* Sign in按鈕 */}
+          {/* <Nav className="nav-login-btn ">
+            <Nav.Link as={NavLink} to="/login">
+              <div className="btn btn-outline-primary">
+                SIGN IN
+              </div>
+            </Nav.Link>
+            {auth && (
+              <Nav.Link as={NavLink} to="/member">
+                會員專區
+              </Nav.Link>
+            )}
+          </Nav> */}
+
+          {/* 登入後 */}
+          {/* 會員專區 下拉式選單 */}
           <Form className="d-flex">
-            <div className="avatar bg-dark rounded-circle mx-2"></div>
-            <NavDropdown>
-              123
-              <hr />
-              456
-              <hr />
-              登出
+            <NavDropdown title="會員專區" id="collasible-nav-dropdown">
+              <NavDropdown.Item>
+                <p>HI, 毛毛日記 <br></br>
+                  您有 3 則提醒未讀</p>
+              </NavDropdown.Item>
+              {/* 主要資訊功能 */}
+              <NavDropdown.Item as={NavLink} to="/member">
+                查看會員專區
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/member/MemberData">
+                會員資料
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/member/PetList">
+                毛孩資料
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/member/OrderHistory">
+                選物紀錄
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/member/CommunityHistory">
+                社群紀錄
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/member/AssistanceHistory">
+                互助紀錄
+              </NavDropdown.Item>
+            {/* 登出按鈕 */}
+              <Nav.Link as={NavLink} to="">
+                登出
+              </Nav.Link>
             </NavDropdown>
             <div className="d-inline-block"></div>
+            {/* 會員頭像 */}
+            <Nav.Link as={NavLink} to="/member/MemberData">
+            <div className="avatar rounded-circle mx-2"></div>
+            </Nav.Link>
           </Form>
         </Navbar.Collapse>
       </Navbar>
