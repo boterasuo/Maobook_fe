aba1d9d084a30e2ed2deb552502556f3a0b731fc
dummiss <dummiss1216@gmail.com>
2022-02-18 23:51:45 +0800

    0218 切版done

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import Intro from "./Store/Intro";
+import Recom from "./Store/Recom";
+import ProductList from "./Store/ProductList";
+import FAQ from "./Store/FAQ";
+import './Store/style/style.scss';
 
-function Store(props) {
-  return (
-    <div>Store</div>
-  )
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+function Store (props) {
+
+
+    return (
+        <Container>
+            <Intro />
+            <Recom />
+            <ProductList />
+            <FAQ />
+        </Container>
+    )
 }
 
-
-export default Store
+export default Store;
---
+++ src/pages/Store/ProductList.js
@@ -0,0 +1,111 @@
+import '../Store/style/ProductListStyle.scss';
+import React from 'react'
+import { Col, Row, Accordion, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+//元件
+import ProductItem from "./components/ProductItem"
+
+//圖片
+import searchIcon from "./storePic/searchIcon.svg";
+import dogIcon from "./storePic/dogIcon.svg";
+import catIcon from "./storePic/catIcon.svg";
+import Checkbox from "./components/Checkbox";
+import Pagination from "./components/Pagination";
+
+function ProductList () {
+    return (
+        <>
+            <section className='wrap1'>
+                <div className='btnarea mb-5 d-flex justify-content-between'>
+                    <div className='protext1 bg-white'>毛孩選物指南</div>
+                    <button className='select-btn'>吃飯飯</button>
+                    <button className='select-btn'>吃點心</button>
+                    <button className='select-btn'>玩玩具</button>
+                    <form>
+                        <input className='search' type="text" name="search" placeholder="找什麼呢" />
+                        <button type="submit"><img className="searchIcon" src={searchIcon} alt="searchIcon" /></button>
+                    </form>
+                </div>
+            </section>
+
+
+            <section className='mt-4' >
+                <Row className='wrap2' xs={1} md={2}>
+                    <Col xs={6} md={3}>
+                        <Row xs={1} md={1} className='d-flex mt-5'>
+                            <Col className='d-flex justify-content-center' >  {/* 狗貓Btn*/}
+                                <button className="mr-4"><img className="dogIcon" src={dogIcon} alt="dogIcon" /></button>
+                                <button ><img className="catIcon" src={catIcon} alt="catIcon" /></button>
+                            </Col>
+                            <Col className=' d-flex mt-3 justify-content-center' > {/* 分類區*/}
+
+                                <Accordion className='categoryArea pt-3' defaultActiveKey="0">
+                                    <Card className='categoryCard'>
+                                        <Accordion.Toggle className='categoryHeader' as={Card.Header} eventKey="0">
+                                            年齡
+                                        </Accordion.Toggle>
+                                        <Accordion.Collapse eventKey="0">
+                                            <Card.Body className='categoryBody'><Checkbox /></Card.Body>
+                                        </Accordion.Collapse>
+                                    </Card>
+                                    <Card className='categoryCard'>
+                                        <Accordion.Toggle className='categoryHeader' as={Card.Header} eventKey="1">
+                                            品牌
+                                        </Accordion.Toggle>
+                                        <Accordion.Collapse eventKey="1">
+                                            <Card.Body className='categoryBody'><Checkbox /></Card.Body>
+                                        </Accordion.Collapse>
+                                    </Card>
+                                    <Card className='categoryCard'>
+                                        <Accordion.Toggle className='categoryHeader' as={Card.Header} eventKey="2">
+                                            別類
+                                        </Accordion.Toggle>
+                                        <Accordion.Collapse eventKey="2">
+                                            <Card.Body className='categoryBody'><Checkbox /></Card.Body>
+                                        </Accordion.Collapse>
+                                    </Card>
+                                </Accordion>
+
+                            </Col>
+                            <Col> {/* 分頁區*/}
+                                <div className='pages'><Pagination /></div>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col xs={12} md={6}>    {/* 商品列表*/}
+                        <Row xs={2} md={3}>
+                            <Col className="mb-5" >
+                                <ProductItem />
+                            </Col>
+                            <Col className="mb-5" >
+                                <ProductItem />
+                            </Col>
+                            <Col className="mb-5" >
+                                <ProductItem />
+                            </Col>
+                            <Col className="mb-5" >
+                                <ProductItem />
+                            </Col>
+                            <Col className="mb-5" >
+                                <ProductItem />
+                            </Col>
+                            <Col className="mb-5" >
+                                <ProductItem />
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </section>
+
+
+
+
+        </>
+
+
+
+    )
+}
+
+export default ProductList;
