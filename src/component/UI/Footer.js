92f034576a98c3cc48ca9d3d39b75535d5eec0f2
Ben-Buli <84383897+Ben-Buli@users.noreply.github.com>
2022-02-18 16:53:43 +0800

    <Footer/>  => anouncement link

--- src/component/UI/Footer.js
+++ src/component/UI/Footer.js
@@ -3,10 +3,10 @@ import React from 'react'
 function Footer(props) {
   return (
     <>
-      <footer className="bg-secondary mt-auto py-3 text-center">
+      <footer className="bg-secondary mt-auto py-3 text-center" style={{ZIndex:9999}}>
         <div className="container">
           <span className="text-white">
-          © 2022 Maobook  作業版權免責說明
+          © 2022 Maobook <a className='text-white' href='#'>作業版權免責說明</a>
           </span>
         </div>
       </footer>
