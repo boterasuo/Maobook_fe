5137d69f9fd3d0fdf47e896deea0140987352078
Eutene Lin <eutenelin@gmail.com>
2022-02-25 20:32:27 +0800

    schedule prettier

--- src/pages/Schedule/Notes.js
+++ src/pages/Schedule/Notes.js
@@ -1,164 +1,157 @@
 // 引入 React 功能
-import React from 'react';
-import { useState, useEffect } from "react";
-import axios from "axios";
+import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 // 引入 utils
 // import {API_URL} from "../../utils/config";
-import 'react-bootstrap';
+import 'react-bootstrap'
 import 'bootstrap'
 import './Notes.scss'
-import {
-  format
-} from 'date-fns'
+import { format } from 'date-fns'
 
 // 引入圖片
-import star from './img/scheduleIcon5.svg' 
+import star from './img/scheduleIcon5.svg'
 // import event1 from './img/event1.svg';
-import event9 from './img/event9.svg';
-import event0 from './img/event0.svg';
+import event9 from './img/event9.svg'
+import event0 from './img/event0.svg'
 // import event5 from './img/event5.svg';
 
-
-function Notes({NoteDate}) {
+function Notes({ NoteDate }) {
   // const {notedate, setNotedate}=props;
-   const [data, setData] = useState([])
-   console.log(NoteDate) 
+  const [data, setData] = useState([])
+  console.log(NoteDate)
 
   // API
-    const queryEvent = async () => {
-        let response = await axios.get("http://localhost:3002/api/calenderNote/"+ NoteDate.getFullYear()+"/"+(NoteDate.getMonth()+1));
-        setData(response.data); 
-    }
-    useEffect(() => {
-        queryEvent();
-    },[NoteDate]);
-    let CheckData = data.filter(a => { 
-      return a.DATE == NoteDate.getDate();
+  const queryEvent = async () => {
+    let response = await axios.get(
+      'http://localhost:3002/api/calenderNote/' +
+        NoteDate.getFullYear() +
+        '/' +
+        (NoteDate.getMonth() + 1)
+    )
+    setData(response.data)
+  }
+  useEffect(() => {
+    queryEvent()
+  }, [NoteDate])
+  let CheckData = data.filter((a) => {
+    return a.DATE == NoteDate.getDate()
   })
-
-    
 
   return (
     <>
-
-    
-        <div className="scheduleNotes">
-          <div className="scheduleNotesStar">
-            <div className="scheduleNotesStarTitle">
-              <img src={star} className="notesStarImg"/>
-              <h3 className="scheduleNotesStarH1" bg-light border>本月重要記事</h3>
-            </div>
-            <div className="scheduleNotesStarbox">
-              <div className="scheduleEvents">
-
-
-              {
-                data.length==0?<div><b className="restTime">目前沒有重要記事喔</b></div>:
-                data.map((item,i) => {
-              return (
-                <>
-                  <div className="asd">
-                      <div className="scheduleEvents1">
-                        <p className="scheduleEventDay">
-                        {item.DATE}
-                        </p>
-                        {/* <img src={event1} className="events1Img" /> */}
-                        </div>
-                      <div className="scheduleEvents2">
-                        <img src={event9}  className="scheduleEvents2Img" />
-                        </div>
-                      <div className="scheduleEvents3">
-                        <img src={event0} />{item.importer}
-                        </div>
-                        {(item.tags).map((tag,i)=> {
-                          return (
-                          
-                      <div className="scheduleEvents4 d-flex">
-                            <div className="bbb">
-                              <div className="scheduleEvents4tagsText">{tag}</div>
-                              {/* <img src={event5} className="scheduleEvents4tags"/> */}
-                            </div>
-                          
-                      </div>
-                          )
-                         })}
-                      <div className="aaaaaa"></div>
-                      <div className="scheduleEvents5">
-                        {item.title}
-                      </div>
-
-                      <br/>
-                
-                   </div>
-                </>
-              )
-              })}
-             
-
-              </div>
-
-
-            </div>
+      <div className="scheduleNotes">
+        <div className="scheduleNotesStar">
+          <div className="scheduleNotesStarTitle">
+            <img src={star} className="notesStarImg" />
+            <h3 className="scheduleNotesStarH1" bg-light border>
+              本月重要記事
+            </h3>
           </div>
-
-          <div ></div>
-
-          <div className="scheduleNotesToday">
-            <h3 className="scheduleNotesTodayTitle"> 當日行程 {format(NoteDate,'yyyy/MM/dd')}</h3>
-            <div className="scheduleNotesTodaybox">
-              <div>
-              {
-                CheckData.length==0?<div><b className="restTime">本日沒有行程喔～快去安排吧！</b></div>:
-                CheckData.map((Dateitem,i) => {
-
-              return (
-                <>
- 
-                  <div className="asd">
-                      <div className="scheduleEvents1">
-                        <p className="scheduleEventDay">
-                        </p>
-                        {/* <img src={event1} className="events1Img" /> */}
+          <div className="scheduleNotesStarbox">
+            <div className="scheduleEvents">
+              {data.length == 0 ? (
+                <div>
+                  <b className="restTime">目前沒有重要記事喔</b>
+                </div>
+              ) : (
+                data.map((item, i) => {
+                  return (
+                    <>
+                      <div className="asd">
+                        <div className="scheduleEvents1">
+                          <p className="scheduleEventDay">{item.DATE}</p>
+                          {/* <img src={event1} className="events1Img" /> */}
                         </div>
-                      <div className="scheduleEvents2">
-                        <img src={event9}  className="scheduleEvents2Img" />
+                        <div className="scheduleEvents2">
+                          <img src={event9} className="scheduleEvents2Img" />
                         </div>
-                      <div className="scheduleEvents3">
-                        <img src={event0} />{Dateitem.importance}
+                        <div className="scheduleEvents3">
+                          <img src={event0} />
+                          {item.importer}
                         </div>
-                      <div className="scheduleEvents4">
-                        {(Dateitem.tags).map((Datetag,i)=> {
+                        {item.tags.map((tag, i) => {
                           return (
-                          <>
-                            <div className="bbb">
-                             {/* <img src={event5} className="scheduleEvents4tags"/> */}
-                             <div className="scheduleEvents4tagsText">{Datetag}</div>
+                            <div className="scheduleEvents4 d-flex">
+                              <div className="bbb">
+                                <div className="scheduleEvents4tagsText">
+                                  {tag}
+                                </div>
+                                {/* <img src={event5} className="scheduleEvents4tags"/> */}
+                              </div>
                             </div>
-                          </>
                           )
-                         })}
+                        })}
+                        <div className="aaaaaa"></div>
+                        <div className="scheduleEvents5">{item.title}</div>
+
+                        <br />
                       </div>
-                      <div className="aaaaaa"></div>
-                      <div className="scheduleEvents5">
-                        {Dateitem.title}
-                      </div>
-
-                      <br/>
-                
-                   </div>
-                </>
-              )
-              })
-              }
-
-
-
-              </div>
-
+                    </>
+                  )
+                })
+              )}
             </div>
           </div>
         </div>
+
+        <div></div>
+
+        <div className="scheduleNotesToday">
+          <h3 className="scheduleNotesTodayTitle">
+            {' '}
+            當日行程 {format(NoteDate, 'yyyy/MM/dd')}
+          </h3>
+          <div className="scheduleNotesTodaybox">
+            <div>
+              {CheckData.length == 0 ? (
+                <div>
+                  <b className="restTime">本日沒有行程喔～快去安排吧！</b>
+                </div>
+              ) : (
+                CheckData.map((Dateitem, i) => {
+                  return (
+                    <>
+                      <div className="asd">
+                        <div className="scheduleEvents1">
+                          <p className="scheduleEventDay"></p>
+                          {/* <img src={event1} className="events1Img" /> */}
+                        </div>
+                        <div className="scheduleEvents2">
+                          <img src={event9} className="scheduleEvents2Img" />
+                        </div>
+                        <div className="scheduleEvents3">
+                          <img src={event0} />
+                          {Dateitem.importance}
+                        </div>
+                        <div className="scheduleEvents4">
+                          {Dateitem.tags.map((Datetag, i) => {
+                            return (
+                              <>
+                                <div className="bbb">
+                                  {/* <img src={event5} className="scheduleEvents4tags"/> */}
+                                  <div className="scheduleEvents4tagsText">
+                                    {Datetag}
+                                  </div>
+                                </div>
+                              </>
+                            )
+                          })}
+                        </div>
+                        <div className="aaaaaa"></div>
+                        <div className="scheduleEvents5">{Dateitem.title}</div>
+
+                        <br />
+                      </div>
+                    </>
+                  )
+                })
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/pages/Schedule/Post.js
+++ src/pages/Schedule/Post.js
@@ -1,8 +1,8 @@
 // 引入 React 功能
-import React from 'react';
-import { useEffect, useState } from "react";
+import React from 'react'
+import { useEffect, useState } from 'react'
 // import axios from 'axios';
-import 'react-bootstrap';
+import 'react-bootstrap'
 
 // 引入 utils
 // import {API_URL} from "../../utils/config";
@@ -11,9 +11,7 @@ import 'react-bootstrap';
 import '../Schedule/Post.scss'
 import scheduleIcon6 from './img/scheduleIcon6.svg'
 
-
 function Post(props) {
-  
   // 日期
   const [date, setDate] = useState('')
   // 重要普通 radio
@@ -30,168 +28,222 @@ function Post(props) {
 
   return (
     <>
-    <h1 className="schedulePostH1">重要的事情</h1>
+      <h1 className="schedulePostH1">重要的事情</h1>
 
-    <div className="bg-primary position-relative">
-      <div className="schedulePost"> 
-        <h2 className="schedulePostH2">寫下來提醒自己吧</h2>
-            <input 
-              className="scheduleData" 
-              type="date" 
-              name="data"
-              value={date}
-              onChange={(e) => {setDate(e.target.value)}}
-              />
+      <div className="bg-primary position-relative">
+        <div className="schedulePost">
+          <h2 className="schedulePostH2">寫下來提醒自己吧</h2>
+          <input
+            className="scheduleData"
+            type="date"
+            name="data"
+            value={date}
+            onChange={(e) => {
+              setDate(e.target.value)
+            }}
+          />
 
-        <form className="schedulePostInput">
-          <div className="schedulePostDate"> 
-            <img src={scheduleIcon6} className="scheduleIcon6" alt=""/>  
-          </div>
-
-          {/* 重要與普通radio */}
-          <div className="schedulePostClass">
-            <fieldset id="importent">
-            <label for="0" className="schedulePostButton1 lg">
-              <input 
-                type="radio" 
-                id="0"
-                name="importent" 
-                value={0}
-                check={'0' === important}
-                onChange={(e) => {setImportant(e.target.value)}}
-                className="schedulePostCircle"/>
-              <span className="schedulePostButtontext">重 要</span>
-            </label>
-
-            <label for="1" className="schedulePostButton2 lg">
-              <input 
-                type="radio" 
-                id="1"
-                name="importent" 
-                value={1}
-                check={'1' === important}
-                onChange={(e) => {setImportant(e.target.value)}}
-                className="schedulePostCircle"/>
-              <span className="schedulePostButtontext">普 通</span>
-            </label>
-              </fieldset>
-          </div>
-
-          <div className="schedulePostTag">
-            <div className="schedulePostTag1">
-              <div class="dropdown">
-              {/* 選擇寵物 */}
-                <select 
-                  name="pets"
-                  className="takeMaoOut" 
-                  type="select" 
-                  value={selectedValue}
-                  onChange={(e) => {setSelectedValue(e.target.value)}}
-                  data-toggle="dropdown" 
-                  aria-haspopup="true" 
-                  aria-expanded="false">
-                  <option value="小喵" className="dropdown-item">小喵</option>
-                  <option value="布魯托" className="dropdown-item">布魯托</option>
-                  <option value="唐基柯德" className="dropdown-item">唐基柯德</option>
-                </select>
-              </div>
-              {/* 輸入TAG標籤 */}
-              <div>
-                <label for="fname" className="schedulePostLabel">＃</label>
-                <input 
-                  className="scheduleInput" 
-                  type="text" 
-                  value={tagOne}
-                  onChange={(e) => {setTagOne(e.target.value)}}
-                  id="fname" 
-                  name="fname" />
-                <br/>
-                <label for="lname" className="schedulePostLabel">＃</label>
-                <input 
-                  className="scheduleInput" 
-                  type="text" 
-                  value={tagTwo}
-                  onChange={(e) => {setTagTwo(e.target.value)}}
-                  id="lname" 
-                  name="lname" />
-              </div>
-
+          <form className="schedulePostInput">
+            <div className="schedulePostDate">
+              <img src={scheduleIcon6} className="scheduleIcon6" alt="" />
             </div>
-            {/* 選擇事件種類 */}
-            <div className="schedulePostTag2">
-            <fieldset id="category">
-                <label for="a" className="scheduleButIcon1 lg">
-                  <input 
-                    type="radio" 
-                    id="a"
-                    name="category" 
+
+            {/* 重要與普通radio */}
+            <div className="schedulePostClass">
+              <fieldset id="importent">
+                <label for="0" className="schedulePostButton1 lg">
+                  <input
+                    type="radio"
+                    id="0"
+                    name="importent"
+                    value={0}
+                    check={'0' === important}
+                    onChange={(e) => {
+                      setImportant(e.target.value)
+                    }}
+                    className="schedulePostCircle"
+                  />
+                  <span className="schedulePostButtontext">重 要</span>
+                </label>
+
+                <label for="1" className="schedulePostButton2 lg">
+                  <input
+                    type="radio"
+                    id="1"
+                    name="importent"
                     value={1}
-                    check={'1' === eventTag}
-                    onChange={(e) => {setEventTag(e.target.value)}}
-                    className="schedulePostCircle"/>
-                  <span className="schedulePostTagtext">疫苗<br/>保健</span>
-                </label>
-
-                <label for="2" className="scheduleButIcon2 lg">
-                  <input 
-                    type="radio" 
-                    id="2"
-                    name="category" 
-                    value={2}
-                    check={'2' === eventTag}
-                    onChange={(e) => {setEventTag(e.target.value)}}
-                    className="schedulePostCircle"/>
-                  <span className="schedulePostTagtext">定期<br/>美容</span>
-                </label>
-
-                <label for="3" className="scheduleButIcon3 lg">
-                  <input 
-                    type="radio" 
-                    id="3"
-                    name="category" 
-                    value={3}
-                    check={'3' === eventTag}
-                    onChange={(e) => {setEventTag(e.target.value)}}
-                    className="schedulePostCircle"/>
-                  <span className="schedulePostTagtext">補貨<br/>罐罐</span>
-                </label>
-
-                <label for="4" className="scheduleButIcon4 lg">
-                  <input 
-                    type="radio" 
-                    id="4"
-                    name="category" 
-                    value={4}
-                    check={'4' === eventTag}
-                    onChange={(e) => {setEventTag(e.target.value)}}
-                    className="schedulePostCircle"/>
-                  <span className="schedulePostTagtext">紀錄<br/>提醒</span>
+                    check={'1' === important}
+                    onChange={(e) => {
+                      setImportant(e.target.value)
+                    }}
+                    className="schedulePostCircle"
+                  />
+                  <span className="schedulePostButtontext">普 通</span>
                 </label>
               </fieldset>
-
             </div>
-          </div>
-      
-          <div className="postSaySomethingDiv">
-            <div className="postSaySomething">
-              <div>
-                <textarea 
-                className="scheduleTextarea"
-                value={textareaValue}
-                onChange={(e) => {setTextareaValue(e.target.value)}}
-                 >寫一些記錄吧！</textarea>
+
+            <div className="schedulePostTag">
+              <div className="schedulePostTag1">
+                <div class="dropdown">
+                  {/* 選擇寵物 */}
+                  <select
+                    name="pets"
+                    className="takeMaoOut"
+                    type="select"
+                    value={selectedValue}
+                    onChange={(e) => {
+                      setSelectedValue(e.target.value)
+                    }}
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <option value="小喵" className="dropdown-item">
+                      小喵
+                    </option>
+                    <option value="布魯托" className="dropdown-item">
+                      布魯托
+                    </option>
+                    <option value="唐基柯德" className="dropdown-item">
+                      唐基柯德
+                    </option>
+                  </select>
+                </div>
+                {/* 輸入TAG標籤 */}
+                <div>
+                  <label for="fname" className="schedulePostLabel">
+                    ＃
+                  </label>
+                  <input
+                    className="scheduleInput"
+                    type="text"
+                    value={tagOne}
+                    onChange={(e) => {
+                      setTagOne(e.target.value)
+                    }}
+                    id="fname"
+                    name="fname"
+                  />
+                  <br />
+                  <label for="lname" className="schedulePostLabel">
+                    ＃
+                  </label>
+                  <input
+                    className="scheduleInput"
+                    type="text"
+                    value={tagTwo}
+                    onChange={(e) => {
+                      setTagTwo(e.target.value)
+                    }}
+                    id="lname"
+                    name="lname"
+                  />
+                </div>
+              </div>
+              {/* 選擇事件種類 */}
+              <div className="schedulePostTag2">
+                <fieldset id="category">
+                  <label for="a" className="scheduleButIcon1 lg">
+                    <input
+                      type="radio"
+                      id="a"
+                      name="category"
+                      value={1}
+                      check={'1' === eventTag}
+                      onChange={(e) => {
+                        setEventTag(e.target.value)
+                      }}
+                      className="schedulePostCircle"
+                    />
+                    <span className="schedulePostTagtext">
+                      疫苗
+                      <br />
+                      保健
+                    </span>
+                  </label>
+
+                  <label for="2" className="scheduleButIcon2 lg">
+                    <input
+                      type="radio"
+                      id="2"
+                      name="category"
+                      value={2}
+                      check={'2' === eventTag}
+                      onChange={(e) => {
+                        setEventTag(e.target.value)
+                      }}
+                      className="schedulePostCircle"
+                    />
+                    <span className="schedulePostTagtext">
+                      定期
+                      <br />
+                      美容
+                    </span>
+                  </label>
+
+                  <label for="3" className="scheduleButIcon3 lg">
+                    <input
+                      type="radio"
+                      id="3"
+                      name="category"
+                      value={3}
+                      check={'3' === eventTag}
+                      onChange={(e) => {
+                        setEventTag(e.target.value)
+                      }}
+                      className="schedulePostCircle"
+                    />
+                    <span className="schedulePostTagtext">
+                      補貨
+                      <br />
+                      罐罐
+                    </span>
+                  </label>
+
+                  <label for="4" className="scheduleButIcon4 lg">
+                    <input
+                      type="radio"
+                      id="4"
+                      name="category"
+                      value={4}
+                      check={'4' === eventTag}
+                      onChange={(e) => {
+                        setEventTag(e.target.value)
+                      }}
+                      className="schedulePostCircle"
+                    />
+                    <span className="schedulePostTagtext">
+                      紀錄
+                      <br />
+                      提醒
+                    </span>
+                  </label>
+                </fieldset>
               </div>
             </div>
-            {/* <div className="buttonDiv"> */}
-              <button className="scheduleSummitButton">送  出</button>
-            {/* </div> */}
-          </div>
 
-
-
-        </form>
+            <div className="postSaySomethingDiv">
+              <div className="postSaySomething">
+                <div>
+                  <textarea
+                    className="scheduleTextarea"
+                    value={textareaValue}
+                    onChange={(e) => {
+                      setTextareaValue(e.target.value)
+                    }}
+                  >
+                    寫一些記錄吧！
+                  </textarea>
+                </div>
+              </div>
+              {/* <div className="buttonDiv"> */}
+              <button className="scheduleSummitButton">送 出</button>
+              {/* </div> */}
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     </>
   )
 }
