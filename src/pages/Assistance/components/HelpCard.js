73af514d090dfbbc85bb06f55c5d657c24aeff3f
Chris <tanghsuwei@gmail.com>
2022-02-25 13:25:01 +0800

    [0225] import api and its variables

--- src/pages/Assistance/components/HelpCard.js
+++ src/pages/Assistance/components/HelpCard.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import "./HelpCard.scss";
@@ -7,7 +8,20 @@ import Hashtag from "./HelpTag";
 // 插圖
 import example from "../img/example.svg";
 
+
 function HelpCard(props) {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    let getHelpList = async () => {
+        let response = await axios.get("http://localhost:3000/api/helplist/");
+      //("${API_URL}/helpdetails/");
+      setData(response.data);
+    };
+    getHelpList();
+  },[]);
+
   return (
     <div className="helpcard d-inline-block">
 
@@ -31,17 +45,17 @@ function HelpCard(props) {
         {/* 圖片 */}
         <div className="imgandregion">
           <img className="card-img" src={example} alt=""/>
-          <div className="cardregion">台北市</div>
+          <div className="cardregion">台北市{data.region}</div>
         </div>
         {/* 內文 */}
         <div className="card-content pt-3 px-3">
-          我是標題標題標題......
+          我是標題標題標題......{data.title}
         </div>
       </div>
 
       {/* [[[價格]]] */}
       <div className="card-footer d-line-block text-center my-3">
-        <div className="cardprice">$ 1000</div>
+        <div className="cardprice">$ 1000{data.price}</div>
       </div>
     </div>
   );
