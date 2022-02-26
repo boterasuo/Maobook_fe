09cce643ad96c8c27bb53abf92b320a0ade3f62b
dummiss <dummiss1216@gmail.com>
2022-02-26 16:32:34 +0800

    0226update 更新搜尋等小功能

--- src/pages/Store/Recom.js
+++ src/pages/Store/Recom.js
@@ -1,8 +1,9 @@
 // import { Card, Button } from 'react-bootstrap';
 // import { Link } from 'react-router-dom';
 import '../Store/style/RecomStyle.scss';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useState,useEffect } from 'react';
+import { API_URL } from "./../../utils/config";
 //元件
 import ProductItem from "./components/ProductItem"
 //圖片
@@ -11,7 +12,44 @@ import weRecom from "./storePic/weRecom.svg";
 // import productCartIcon from "./storePic/productCartIcon.svg";
 
 
-function Recom () {
+function Recom() {
+
+    const [data, setData] = useState([]); //紀錄全部寵物資料
+    const [pet, setPet] = useState(null); //紀錄單隻寵物因為要塞進去的資料是物件，不能設陣列
+
+    //寵物清單API 開始
+    useEffect(() => {
+        let getPetlist = async () => {
+            // http://localhost:3002//api/store/petlist
+            let response = await axios.get(`${API_URL}/store/petlist`, { withCredentials: true, });
+            setData(response.data);
+            setPet(response.data[0]);
+            console.log("aaa", response.data);
+        };
+        getPetlist();
+    }, []);
+
+
+    const getlist = () => { //建立寵物清單元件
+        let list = [];
+        for (let i = 0; i < data.length; i++) {
+            list.push(
+                <li key={i}
+                    onClick={(e) => {
+                        setPet(data[i]); //點下後針對index做寵物切換
+                    }}><button className="recom-pet">{i}</button></li>
+            );
+        }
+        return list;
+    };
+
+    // const showPet  = () => {
+
+    // };
+    //寵物清單API 結束
+
+
+
     return (
         <>
             <section className="d-flex justify-content-center">
@@ -20,28 +58,35 @@ function Recom () {
                         <div>今天的你</div>
                         <div>想為哪個毛孩買禮物呢</div>
                     </div>
+
                     <ul className="allpet list-unstyled d-flex m-0">
-                        <li><button className="recom-pet"></button></li>
-                        <li><button className="recom-pet"></button></li>
-                        <li><button className="recom-pet"></button></li>
+                        {getlist()}  {/*印出寵物清單元件*/}
                     </ul>
                 </div>
             </section>
-
             <section className="d-flex justify-content-center">
                 <div className="recomArea d-flex">
                     <div className="mainpet ">
                         <div className="recom-avatar"></div>
-                        <div className="recom-petname text-center mb-3 ">汪汪</div>
-                        <div className="recom-describe text-center ">今天的牠四歲<br />是隻成年的小狗狗<br />過去曾有看過獸醫<br />有著過肥、不愛喝水的問題</div>
+                        <div className="recom-petname text-center mb-3 ">{pet ? pet.name : ''}</div>
+                        <div className="recom-describe text-center ">
+                            今天的牠<span className='introTag'>{pet ? pet.birthday : ''}歲</span><br />
+                            是隻<span>{pet ? pet.age_category : ''}</span>的<span>{pet ? pet.category : ''}</span><br />
+                            有著
+                            {pet ? (pet.illnessTags.map((data) => {
+                                return (
+                                    <span> [ {data} ] </span>
+                                )
+                            })) : ''} 的問題
+                        </div>
                     </div>
                     <div className="recomlist">
                         <img className="weRecom" src={weRecom} alt="weRecom" />
                         <div className="recomPro d-flex">
                             {/* 推薦商品 */}
-                            <ProductItem/>
-                            <ProductItem/>
-                            <ProductItem/>
+                            <ProductItem />
+                            <ProductItem />
+                            <ProductItem />
                         </div>
                     </div>
                 </div>
--- src/pages/Store/components/Pagination.js
+++ src/pages/Store/components/Pagination.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Pagination} from 'react-bootstrap';
+import { Pagination } from 'react-bootstrap';
 
-let active = 2;
-let items = [];
-for (let number = 1; number <= 5; number++) {
-    items.push(
-        <Pagination.Item key={number} active={number === active}>
-            {number}
-        </Pagination.Item>,
-    );
-}
 
-function paginationBasic() {
+function paginationBasic(props) {
+    const { lastPage,setPage,page } = props;
+    // let active = 2;
+    let items = [];
+    for (let number = 1; number <= lastPage; number++) {
+        items.push(
+            <Pagination.Item key={number} onClick={(e) => {setPage(number)}} active={number === page}>
+                {number}
+            </Pagination.Item>,
+        );
+    }
+    
     return (
         <Pagination size="sm justify-content-center">{items}</Pagination>
     )
