0fc6c45e9fad798e6acd3b4858513293adf5577d
Chris <tanghsuwei@gmail.com>
2022-02-26 12:47:46 +0800

    [0226] 小賴老師: calendar api

--- src/pages/Assistance/HelpCalendar.js
+++ src/pages/Assistance/HelpCalendar.js
@@ -1,137 +1,157 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { format, getDate, isSameDay } from 'date-fns';
-import useCalendar, { WEEKS } from './components/UseCalendar';
-import 'react-bootstrap';
-import './components/HelpCalendar.scss';
-import buttonIconL from './img/calArrowL.svg';
-import buttonIconR from './img/calArrowR.svg';
-import dateCircle from './img/calDateCircle.svg';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { format, getDate, isSameDay } from 'date-fns'
+import useCalendar, { WEEKS } from './components/UseCalendar'
+import 'react-bootstrap'
+import './components/HelpCalendar.scss'
+import buttonIconL from './img/calArrowL.svg'
+import buttonIconR from './img/calArrowR.svg'
+import dateCircle from './img/calDateCircle.svg'
 
-const HelpCalendar = () => {
+const HelpCalendar = (props) => {
+  const calendar = useCalendar()
 
-    const [data, setData] = useState([])
-    const calendar = useCalendar()
+  const [data, setData] = useState([])
 
-    useEffect(() => {
-        let getCaseNum = async () => {
-            let response = await axios.get("http://localhost:3000/api/helpcalendar/"+calendar.today.getFullYear()+"/"+(calendar.today.getMonth()+1));
-          //("${API_URL}/helpcalendar/");
-          setData(response.data);
-        };
-        getCaseNum();
-      },[calendar.today]);
+  useEffect(() => {
+    let getCaseNum = async () => {
+      let response = await axios.get(
+        'http://localhost:3002/api/help/helcalendar' +
+          calendar.today.getFullYear() +
+          '/' +
+          (calendar.today.getMonth() + 1)
+      )
+      console.log(
+        'http://localhost:3002/api/help/helpcalendar' +
+          calendar.today.getFullYear() +
+          '/' +
+          (calendar.today.getMonth() + 1)
+      )
+      //("${API_URL}/helpcalendar/");
+      console.log('response.data', response.data)
+      let map = response.data.reduce((accu, current) => {
+        console.log(accu, current)
+        if (!accu[current.day]) {
+          accu[current.day] = 1
+        } else {
+          accu[current.day]++
+        }
+        return accu
+      }, {})
+      console.log('mapmap', map)
+      setData(map)
+    }
+    getCaseNum()
+  }, [calendar.today])
 
-
-    return (
-        <>
-        <div className="calendar">
-
+  return (
+    <>
+      <div className="calendar">
         {/* 年月 & setState前後按鈕 */}
         <table>
-                <thead>
-                    <tr>
-                        <td colSpan="100%" className="calendarHead">
-                        <div className="d-flex justify-content-center">
+          <thead>
+            <tr>
+              <td colSpan="100%" className="calendarHead">
+                <div className="d-flex justify-content-center">
+                  <img
+                    src={buttonIconL}
+                    className="imgIcon mx-5"
+                    onClick={calendar.setPreMonth}
+                    alt="上一個月"
+                  />
 
-                        <img src={buttonIconL} className="imgIcon mx-5" onClick={calendar.setPreMonth} alt="上一個月"/>
+                  {/* 可替換format: dd(加上日期) MM(數字月) MMMM(完整英文月) */}
+                  <div className="thisYear d-inline-block">
+                    {format(calendar.today, 'MMM yyyy')}
+                  </div>
 
-        {/* 可替換format: dd(加上日期) MM(數字月) MMMM(完整英文月) */}
-                        <div className="thisYear d-inline-block">
-                            {format(calendar.today, 'MMM yyyy')}</div>
-                        
-                        <img src={buttonIconR} className="mx-5" onClick={calendar.setNextMonth} alt="下一個月"/>
-                        </div>
-
-                        </td>
-                    </tr>
-                </thead>
-
-                <br/>
-
-                <tbody>
-
-        {/* 印出星期標頭 */}
-                    <tr className="week">
-                        {WEEKS.map((title, i) => {
-                            return (
-                            <td key={i}>{title}</td>
-                            )
-                        })}                 
-                    </tr>
-                    
-        {/* 分隔線 */}
-                    <tr>
-                    <td colSpan="100%"> <hr className="divider"/> </td>
-                    </tr> 
-
-        {/* 印出日期 */}
-                    {calendar.days.map((week, i) => {
-                        return (
-                            <>
-                            <tr key={i}>
-                            
-                                {week.map((date, i) => {
-                            
-                                    const otherMonth = date.otherMonth
-                                    //const isSelected = isSameDay(calendar.today,date.date)
-                                    //const className = `${
-                                    //    otherMonth && 'other'
-                                    //} ${isSelected && 'selected'}`
-                                    const selectedToday = () => {
-                                        calendar.selectDate(date.date)
-                                    }
-                                    
-                                    const daycases =[]
-                                    let checkdate = data.find(a => { 
-                                    return a.date == getDate(date.date);
-                                    });
-                                    let showdaycases = checkdate === undefined ? [] : checkdate.id;
-                                    for(let i = 0; i < showdaycases.length; i++){
-                                    daycases.push(showdaycases[i])
-                                }
-        
-                                    return (
-                                        <>
-                                        <td
-                                            key={i}
-                                            // className={className}
-                                            className="today"
-                                            onClick={selectedToday}>
-
-                                        <tr>
-                                            <td>{!otherMonth && getDate(date.date)}</td>
-                                        </tr>    
-
-                                        <tr> 
-                                           <td >                                           
-                                           {!otherMonth &&                                            
-                                            <div className="linkCircle">
-                                            <img src={dateCircle} className="dateCircle" alt=""/>
-                                            <div className="circleText">{daycases.length}</div>
-                                            </div>
-                                            }
-                                            </td>
-                                        </tr>
-                                        
-                                        </td>
-                                        
-                                        </>
-                                       
-                                    )
-                                })}
-                            </tr>
-                            
-                            </>
-                        )
-                    })}
-                    
-                </tbody>
-                </table>
+                  <img
+                    src={buttonIconR}
+                    className="mx-5"
+                    onClick={calendar.setNextMonth}
+                    alt="下一個月"
+                  />
                 </div>
-        </>
-    )
+              </td>
+            </tr>
+          </thead>
+
+          <br />
+
+          <tbody>
+            {/* 印出星期標頭 */}
+            <tr className="week">
+              {WEEKS.map((title, i) => {
+                return <td key={i}>{title}</td>
+              })}
+            </tr>
+
+            {/* 分隔線 */}
+            <tr>
+              <td colSpan="100%">
+                {' '}
+                <hr className="divider" />{' '}
+              </td>
+            </tr>
+
+            {/* 印出日期 */}
+            {calendar.days.map((week, i) => {
+              return (
+                <>
+                  <tr key={i}>
+                    {week.map((date, i) => {
+                      const otherMonth = date.otherMonth
+                      //const isSelected = isSameDay(calendar.today,date.date)
+                      //const className = `${
+                      //    otherMonth && 'other'
+                      //} ${isSelected && 'selected'}`
+                      const selectedToday = () => {
+                        calendar.selectDate(date.date)
+                      }
+
+                      return (
+                        <>
+                          <td
+                            key={i}
+                            // className={className}
+                            className="today"
+                            onClick={selectedToday}
+                          >
+                            <tr>
+                              <td>{!otherMonth && getDate(date.date)}</td>
+                            </tr>
+
+                            <tr>
+                              <td>
+                                {!otherMonth && (
+                                  <div className="linkCircle">
+                                    <img
+                                      src={dateCircle}
+                                      className="dateCircle"
+                                      alt=""
+                                    />
+                                    <div className="circleText">
+                                      {data[getDate(date.date)]
+                                        ? data[getDate(date.date)]
+                                        : 0}
+                                    </div>
+                                  </div>
+                                )}
+                              </td>
+                            </tr>
+                          </td>
+                        </>
+                      )
+                    })}
+                  </tr>
+                </>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
 }
 
-
-export default HelpCalendar;
+export default HelpCalendar
--- src/pages/Assistance/components/HelpCard.js
+++ src/pages/Assistance/components/HelpCard.js
@@ -1,30 +1,27 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import PropTypes from "prop-types";
-import { Card, Button, Container, Row, Col } from "react-bootstrap";
-import "./HelpCard.scss";
-import Hashtag from "./HelpTag";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import PropTypes from 'prop-types'
+import { Card, Button, Container, Row, Col } from 'react-bootstrap'
+import './HelpCard.scss'
+import Hashtag from './HelpTag'
 
 // 插圖
-import example from "../img/example.svg";
-
+import example from '../img/example.svg'
 
 function HelpCard(props) {
-
   const [data, setData] = useState([])
 
-  useEffect(() => {
-    let getHelpList = async () => {
-        let response = await axios.get("http://localhost:3000/api/helplist/");
-      //("${API_URL}/helpdetails/");
-      setData(response.data);
-    };
-    getHelpList();
-  },[]);
+  // useEffect(() => {
+  //   let getHelpList = async () => {
+  //       let response = await axios.get("http://localhost:3000/api/helplist/");
+  //     //("${API_URL}/helpdetails/");
+  //     setData(response.data);
+  //   };
+  //   getHelpList();
+  // },[]);
 
   return (
     <div className="helpcard d-inline-block">
-
       {/* [[[頭像、標籤Tag]]] */}
       <Container className="px-2">
         <Row>
@@ -32,10 +29,10 @@ function HelpCard(props) {
             <div className="helpavatar rounded-circle bg-secondary"></div>
           </Col>
 
-        <Col xs={1} sm={1} md={1} lg={1}></Col>
+          <Col xs={1} sm={1} md={1} lg={1}></Col>
 
           <Col xs={6} sm={6} md={6} lg={6}>
-            <Hashtag style={{ width:'100%'}} className="hash-tag" />
+            <Hashtag style={{ width: '100%' }} className="hash-tag" />
           </Col>
         </Row>
       </Container>
@@ -44,23 +41,21 @@ function HelpCard(props) {
       <div className="card-body">
         {/* 圖片 */}
         <div className="imgandregion">
-          <img className="card-img" src={example} alt=""/>
-          <div className="cardregion">台北市{data.region}</div>
+          <img className="card-img" src={example} alt="" />
+          <div className="cardregion">台北市</div>
         </div>
         {/* 內文 */}
-        <div className="card-content pt-3 px-3">
-          我是標題標題標題......{data.title}
-        </div>
+        <div className="card-content pt-3 px-3">我是標題標題標題......</div>
       </div>
 
       {/* [[[價格]]] */}
       <div className="card-footer d-line-block text-center my-3">
-        <div className="cardprice">$ 1000{data.price}</div>
+        <div className="cardprice">$ 1000</div>
       </div>
     </div>
-  );
+  )
 }
 
-HelpCard.propTypes = {};
+HelpCard.propTypes = {}
 
-export default HelpCard;
+export default HelpCard
