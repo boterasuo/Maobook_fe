5bcb64a483709fbeafddbac1c7186d9b8cf53844
dummiss <dummiss1216@gmail.com>
2022-03-01 07:53:08 +0800

    更新篩選 加入購物車功能 購物車商品顯示

--- src/pages/Store/components/CartProductItem.js
+++ src/pages/Store/components/CartProductItem.js
@@ -1,46 +1,111 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import { useState } from 'react'
-import './CartProductItemStyle.scss';
+import './CartProductItemStyle.scss'
 
 //元件
-import Counter from "./Counter"
-import ProductDetails from "../ProductDetails"
+import Counter from './Counter'
+import ProductDetails from '../ProductDetails'
 //圖片
-import Hill from "../productsImages/Hill’s id=2-1.png";
-import deletIcon from "../storePic/deletIcon.svg";
+import Hill from '../productsImages/Hill’s-001-1.png'
+import deletIcon from '../storePic/deletIcon.svg'
 
+function CartProductItem(props) {
+  const {
+    item,
+    name,
+    price,
+    des,
+    stock,
+    id,
+    amount,
+    updateCartToLocalStorage,
+    deletItem,
+  } = props
 
-function CartProductItem() {
+  //計算數量總和小計
+  const [num, setNum] = useState(0)
+  const [show, setShow] = useState(false) //Modal
 
-    //計算數量總和小計
-    const [num, setNum] = useState(0);
-
-    let proprice = 1000;
-    let total = num * proprice;
-    const [show, setShow] = useState(false); //Modal
-
-    return (
-        <>
-            <div className=" cart-product ">
-                <button className='deletIcon'><img src={deletIcon} alt="deletIcon" /></button>
-                <div className=' justify-content-center'>
-                    <button className="cart-productImg" onClick={() => setShow(true)}>
-                        <img className="Hill cover-fit" src={Hill} alt="Hill" />
-                    </button>
-                    <ProductDetails show={show} setShow={setShow} />
-                    <div >
-                        <h6 className=" cart-title m-0">美味無添加<br />香草貓貓化毛膏</h6>
-                        <p className="cart-price m-0">${proprice}</p>
-                        <p className="cart-price2 m-0">小計 NT${total}</p>
-                    </div>
-                </div>
-                <div className=' d-flex justify-content-around'>
-                    <Counter num={num} setNum={setNum} />  {/*將值傳入屬性*/}
-                </div>
-            </div>
-        </>
-    )
-
+  return (
+    <>
+      {/* 商品*/}
+      <button
+        className="deletIcon"
+        onClick={() => {
+          deletItem(item, true)
+        }}
+      >
+        <img src={deletIcon} alt="deletIcon" />
+      </button>
+      <div>
+        <div className="cart-product mt-4">
+          <button onClick={() => setShow(true)}>
+            <img className="Hill cover-fit" src={Hill} alt="Hill" />
+          </button>
+          <ProductDetails
+            show={show}
+            setShow={setShow}
+            id={id}
+            name={name}
+            price={price}
+            des={des}
+            stock={stock}
+            item={item}
+            amount={amount}
+          />
+        </div>
+        <div>
+          <h6 className="cart-title">{name}</h6>
+          <p className="cart-price">${price}</p>
+          <p className="cart-price2">小計 NT${price * amount}</p>
+        </div>
+      </div>
+      <div className="counter d-flex justify-content-center">
+        <Counter
+          item={item}
+          amount={amount}
+          updateCartToLocalStorage={updateCartToLocalStorage}
+        />{' '}
+        {/*將值傳入屬性*/}
+      </div>
+    </>
+  )
 }
 
-export default CartProductItem;
+export default CartProductItem
+
+//   <>
+// <button className="deletIcon">
+//   <img src={deletIcon} alt="deletIcon" />
+// </button>
+// <div className=" cart-product ">
+//   <div className="  justify-content-center">
+//     <button onClick={() => setShow(true)}>
+//       <img className="Hill cover-fit" src={Hill} alt="Hill" />
+//     </button>
+//     <ProductDetails
+//       show={show}
+//       setShow={setShow}
+//       id={id}
+//       name={name}
+//       price={price}
+//       des={des}
+//       stock={stock}
+//     />
+//   </div>
+//   <div>
+//     <h6 className="cart-title">{name}</h6>
+//     <p className="cart-price">${price}</p>
+//     <p className="cart-price2">小計 NT${price * amount}</p>
+//   </div>
+// </div>
+
+// <div className="counter d-flex justify-content-around">
+//   <Counter
+//     item={item}
+//     amount={amount}
+//     updateCartToLocalStorage={updateCartToLocalStorage}
+//   />{' '}
+
+// </div >
+// </>
